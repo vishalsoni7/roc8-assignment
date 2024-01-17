8b23f5c6e81815f45d92015b3dc6f8735131c638
vishal soni <vishsoni043@gmail.com>
2024-01-17 19:09:51 +0530

    feature - added landing page

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+  return (
+    <div className="navbar">
+      <p> Homepage </p>
+
+      <div>
+        <p> Login </p>
+        <p className="create-account"> Create Account </p>
+      </div>
+    </div>
+  );
+};
---
+++ src/component/Searchbar.jsx
@@ -0,0 +1,12 @@
+export const SearchBar = () => {
+  return (
+    <div className="searchbar-div">
+      <h1>
+        Discover over 2,000,000 <br /> free Stock Images
+      </h1>
+
+      <input />
+      <p> Trending: flowers, love, forest, river </p>
+    </div>
+  );
+};
